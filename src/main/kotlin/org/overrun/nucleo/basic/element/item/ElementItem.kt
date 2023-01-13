54e89beff726575d6aed6a645802fc62d284a0a1
baka4n <qq474899581@gmail.com>
2023-01-13 21:54:01 +0800

    更新 ElementItem.kt

--- src/main/kotlin/org/overrun/nucleo/basic/element/item/ElementItem.kt
+++ src/main/kotlin/org/overrun/nucleo/basic/element/item/ElementItem.kt
@@ -14,45 +14,37 @@ import org.overrun.nucleo.Nucleoplasm
 import org.overrun.nucleo.Nucleoplasm.toTranslationKey
 import java.io.File
 import java.io.FileOutputStream
-import java.security.MessageDigest
 import java.util.*
 import net.minecraft.text.Style.EMPTY as defaultStyle
 
 class ElementItem(settings: Settings) : Item(settings) {
     val decayOut = HashMap<String, ArrayList<ItemStack?>>()
-    val properties: Properties = Properties()
-    val file = FabricLoader.getInstance().configDir.resolve("decay").toFile()
-    val path = File(file, MessageDigest.getInstance("MD5").apply {
-        val apply = MessageDigest.getInstance("MD5").apply {
-            update((Nucleoplasm.NAMESPACE.toByte()))
-        }
-        update(apply.toString().toByte())
-    }.toString())
+    private val properties: Properties = Properties()
+    private val file = FabricLoader.getInstance().configDir.resolve("decay").toFile()
+    private val path = File(file,".db")
     init {
 
         if (!file.exists()) file.mkdirs()
 
         if (!path.exists()) {
             path.createNewFile()
+            properties.store(FileOutputStream(path), null)
         }
         properties.load(path.inputStream())
     }
 
     private fun getUUidDecay(key: String): Int {
-        val instance = MessageDigest.getInstance("MD5").apply {
-            update(key.toByte())
-        }.toString()
-        if (properties.contains(instance)) {
-            return properties.getProperty(instance).toInt()
+
+        if (properties.contains(key)) {
+            return properties.getProperty(key).toInt()
         }
-        return Int.MIN_VALUE
+        return 0
     }
     private fun addAndSave(key:String, value:Int) {
-        val instance = MessageDigest.getInstance("MD5").apply {
-            update(key.toByte())
-        }.toString()
-        properties.setProperty(instance, value.toString())
+
+        properties.setProperty(key, value.toString())
         properties.store(FileOutputStream(path), "by baka4n")
+        properties.load(path.inputStream())
     }
     override fun inventoryTick(stack: ItemStack?, world: World?, entity: Entity?, slot: Int, selected: Boolean) {
 
@@ -62,17 +54,24 @@ class ElementItem(settings: Settings) : Item(settings) {
 //        println(decayJson)
         stack!!.apply {
             compound = nbt!!.copy()//copy nbt
-            compound.apply {
-                if (!contains("decay_uid")) {
-                    val uid = UUID.randomUUID().toString()
-                    putString("decay_uid", uid)
-                    addAndSave(uid, 0)
+            if (compound.contains("decay_uid")) {
+                compound.apply {
+                    putString("decay_uid", UUID.randomUUID().toString())
+                    addAndSave(getString("decay_uid"), 0)
+
                 }
+                nbt = compound // write nbt
+            } else {
+                compound.apply {
+                    if (!properties.containsKey(getString("decay_uid"))) {
+                        addAndSave(getString("decay_uid"), 0)
+                    } else {
+                        addAndSave(getString("decay_uid"), getUUidDecay("decay_uid"))
+                    }
+                }
+
             }
-            nbt = compound // write nbt
-        }
-        stack.apply {
-            compound = nbt!!.copy().apply {
+            compound.apply {
                 if (getBoolean("decay")) {
                     if (properties.containsKey(getString("decay_uid"))) {
                         uidDecay = getUUidDecay(getString("decay_uid"))
@@ -89,57 +88,69 @@ class ElementItem(settings: Settings) : Item(settings) {
             if (compound.getBoolean("decay")) {
                 if (uidDecay >= compound.getDouble("mc_half_life")) {
                     uidDecay = 0
+                    addAndSave(compound.getString("decay_uid"), 0)
+                    val stacks = decayOut["${compound.getInt("proton")}_${compound.getInt("neutron")}"]!!
+                    for (i in 0 until stacks.size) {
+                        if (i == 0) {
+                            nbt = stacks[i]?.nbt?.copy()
+                        } else {
+                            entity?.dropStack(stacks[i])
+                        }
+                    }
                 }
             }
-
-
-//            if (compound.getBoolean("decay") && compound.contains("decay_uid")) {
-//
-//                if (compound.contains("last_decay")) {
-//                    if (!properties.containsKey(compound.getString("decay_uid"))) {
-//                        properties[compound.getString("decay_uid")] = compound.getInt("last_decay")
-//                    }
-//                } else {
-//                    addAndSave(compound.getString("decay_uid"),)
-//
-//                    properties[compound.getString("decay_uid")]?.let { compound.putInt("last_decay", it) }
-//                }
-//            }
-//            if (compound.getBoolean("decay")) {
-//                compound.apply {
-//                    if (!contains("decay_uuid")) {
-//                        putString("decay_uid", UUID.randomUUID().toString())
-//                        properties[getString("decay_uid")] = 0
-//                        putInt("last_decay", getUUidDecay(getString("decay_uid")))
-//                    }
-//
-//                    (if (decayUID.containsKey(getString("decay_uid"))) decayUID[getString("decay_uid")]!! else 0).also { uidDecay = it }
-//                    decayUID[getString("decay_uid")] = uidDecay + 1
-//                    println(uidDecay + 1)
-//
-//                }
-//                if (uidDecay % 50 == 0) {
-//                    compound.putInt("last_decay",uidDecay)
-//                    nbt = compound
-//                }
-//                if (uidDecay >= compound.getDouble("mc_half_life")) {
-//                    val stacks: ArrayList<ItemStack?>
-//                    compound.apply {
-//                        remove("last_decay")
-//                        decayUID[getString("decay_uid")] = 0
-//                        val proton = getInt("proton")
-//                        val neutron = getInt("neutron")
-//                        stacks = decayOut["${proton}_$neutron"]!!
-//                    }
-//                    for (i in 0 until stacks.size) {
-//                        if (i == 0) nbt = stacks[i]?.nbt?.apply {
-//                            putString("decay_uid", compound.getString("decay_uid"))
-//                        }
-//                        else entity?.dropStack(stacks[i])
-//                    }
-//                }
-//            }
         }
+//        stack.apply {
+//
+//
+//
+////            if (compound.getBoolean("decay") && compound.contains("decay_uid")) {
+////
+////                if (compound.contains("last_decay")) {
+////                    if (!properties.containsKey(compound.getString("decay_uid"))) {
+////                        properties[compound.getString("decay_uid")] = compound.getInt("last_decay")
+////                    }
+////                } else {
+////                    addAndSave(compound.getString("decay_uid"),)
+////
+////                    properties[compound.getString("decay_uid")]?.let { compound.putInt("last_decay", it) }
+////                }
+////            }
+////            if (compound.getBoolean("decay")) {
+////                compound.apply {
+////                    if (!contains("decay_uuid")) {
+////                        putString("decay_uid", UUID.randomUUID().toString())
+////                        properties[getString("decay_uid")] = 0
+////                        putInt("last_decay", getUUidDecay(getString("decay_uid")))
+////                    }
+////
+////                    (if (decayUID.containsKey(getString("decay_uid"))) decayUID[getString("decay_uid")]!! else 0).also { uidDecay = it }
+////                    decayUID[getString("decay_uid")] = uidDecay + 1
+////                    println(uidDecay + 1)
+////
+////                }
+////                if (uidDecay % 50 == 0) {
+////                    compound.putInt("last_decay",uidDecay)
+////                    nbt = compound
+////                }
+////                if (uidDecay >= compound.getDouble("mc_half_life")) {
+////                    val stacks: ArrayList<ItemStack?>
+////                    compound.apply {
+////                        remove("last_decay")
+////                        decayUID[getString("decay_uid")] = 0
+////                        val proton = getInt("proton")
+////                        val neutron = getInt("neutron")
+////                        stacks = decayOut["${proton}_$neutron"]!!
+////                    }
+////                    for (i in 0 until stacks.size) {
+////                        if (i == 0) nbt = stacks[i]?.nbt?.apply {
+////                            putString("decay_uid", compound.getString("decay_uid"))
+////                        }
+////                        else entity?.dropStack(stacks[i])
+////                    }
+////                }
+////            }
+//        }
 
         super.inventoryTick(stack, world, entity, slot, selected)
     }
