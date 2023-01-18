72282309a311c91aeaf66381678e06b08825c6be
baka4n <qq474899581@gmail.com>
2023-01-18 22:40:29 +0800

    r
    Co-Authored-By: squid233 <60126026+squid233@users.noreply.github.com>

--- Fabric1.19.3/src/main/java/org/overrun/nucleoplasm/Nucleoplasm.java
+++ Fabric1.19.3/src/main/java/org/overrun/nucleoplasm/Nucleoplasm.java
@@ -1,10 +1,11 @@
 package org.overrun.nucleoplasm;
 
 import net.fabricmc.api.ModInitializer;
-import org.overrun.nucleoplasm.item.ElementItem;
+import org.overrun.nucleoplasm.item.RegItem;
 
 public class Nucleoplasm implements ModInitializer {
     @Override
     public void onInitialize() {
+        RegItem.init(new FabricNRegistry());
     }
 }
