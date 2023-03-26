d7fe34c37331ef8bb2d349802945435e735b027e
baka4n <qq474899581@gmail.com>
2023-03-26 20:45:00 +0800

    推倒重来

--- Basic1.19.3/src/main/java/org/overrun/nucleoplasm/api/elements/all/AllJson.java
+++
@@ -1,9 +0,0 @@
-package org.overrun.nucleoplasm.api.elements.all;
-
-import com.google.gson.annotations.SerializedName;
-
-import java.util.List;
-
-public record AllJson(@SerializedName("elements") List<Name> elements) {
-
-}
