0dc1f1dd718fbe2ef481e7a1d3982be69425f25c
baka4n <qq474899581@gmail.com>
2023-03-30 16:28:01 +0800

    添加主类

--- fabric/nucleoplasm-api/src/main/java/org/overrun/nucleoplasm_api/Nucleoplasm_api.java
+++ fabric/nucleoplasm-api/src/main/java/org/overrun/nucleoplasm_api/Nucleoplasm_api.java
@@ -1,2 +1,10 @@
-public class Nucleoplasm_api {
+package org.overrun.nucleoplasm_api;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nucleoplasm_api implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
 }
--- fabric/nucleoplasm-api/src/main/java/org/overrun/nucleoplasm_api/client/Nucleoplasm_apiClient.java
+++ fabric/nucleoplasm-api/src/main/java/org/overrun/nucleoplasm_api/client/Nucleoplasm_apiClient.java
@@ -1,2 +1,10 @@
-public class Nucleoplasm_apiClient {
+package org.overrun.nucleoplasm_api.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Nucleoplasm_apiClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
