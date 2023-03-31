f82b86c08a4ec817b3323b3e6dce0a8afa060506
baka4n <qq474899581@gmail.com>
2023-03-31 21:56:14 +0800

    修复完成

--- fabric/nucleoplasm-feature-repair/src/main/java/org/overrun/nucleoplasm_feature_repair/mixin/RailMachineRepair.java
+++ fabric/nucleoplasm-feature-repair/src/main/java/org/overrun/nucleoplasm_feature_repair/mixin/RailMachineRepair.java
@@ -1,43 +1,42 @@
 package org.overrun.nucleoplasm_feature_repair.mixin;
 
-import net.minecraft.block.RailBlock;
-import net.minecraft.block.SlimeBlock;
+import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.overrun.nucleoplasm_api.common.Nucleoplasm_api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static org.overrun.nucleoplasm_api.common.Nucleoplasm_api.MODID;
+
 @Mixin(BlockItem.class)
 public class RailMachineRepair {
-    @Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
+    Logger logger = LoggerFactory.getLogger(MODID);
+    @Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;", at = @At("RETURN"), cancellable = true)
     public void place(ItemPlacementContext context, CallbackInfoReturnable<ActionResult> cir) {
         BlockPos blockPos1 = context.getBlockPos();
-        BlockPos blockPos = new BlockPos(blockPos1.getX(), blockPos1.getY(), blockPos1.getZ() - 1);
+        BlockPos blockPos = new BlockPos(blockPos1.getX(), blockPos1.getY()  - 1, blockPos1.getZ());
         PlayerEntity player = context.getPlayer();
-
         if (player != null) {
             World world = player.getWorld();
-            Item item = context.getStack().getItem();
-
-            if (
-                    item.equals(Items.RAIL)
-                    || item.equals(Items.ACTIVATOR_RAIL)
-                    || item.equals(Items.POWERED_RAIL)
-                    || item.equals(Items.DETECTOR_RAIL)
-            ) {
-                if (world.getBlockState(blockPos).getBlock() instanceof SlimeBlock) {
-                    cir.setReturnValue(ActionResult.FAIL);
+            ItemStack stack = context.getStack();
+            BlockItem item = (BlockItem) stack.getItem();
+            if (item.getBlock() instanceof AbstractRailBlock) {
+                if (world.getBlockState(blockPos).getBlock() instanceof SlimeBlock s) {
+                    world.setBlockState(blockPos1, Blocks.AIR.getDefaultState());
+                    player.giveItemStack(stack);
                 }
             }
         }
     }
+
+
 }
 
