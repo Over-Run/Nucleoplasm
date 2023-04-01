6fd4cba75a54acc0bd90ea1ca78e933e1f73a709
baka4n <qq474899581@gmail.com>
2023-04-01 10:52:47 +0800

    It is prohibited to place railway tracks near the mucus block, and it is prohibited to brush wool railway tracks
    It is prohibited to place railway tracks near the mucus block, and it is prohibited to brush wool railway tracks

--- fabric/nucleoplasm-feature-repair/src/main/java/org/overrun/nucleoplasm_feature_repair/mixin/RailMachineRepair.java
+++ fabric/nucleoplasm-feature-repair/src/main/java/org/overrun/nucleoplasm_feature_repair/mixin/RailMachineRepair.java
@@ -2,26 +2,62 @@ package org.overrun.nucleoplasm_feature_repair.mixin;
 
 import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Position;
 import net.minecraft.world.World;
-import org.overrun.nucleoplasm_api.common.Nucleoplasm_api;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import static org.overrun.nucleoplasm_api.common.Nucleoplasm_api.MODID;
-
-@Mixin(PistonBlock.class)
+//禁止粘液块附近放置铁轨，杜绝刷羊毛铁轨
+@Mixin(BlockItem.class)
 public class RailMachineRepair {
-    @Inject(method = "tryMove", at = @At("HEAD"))
-    public void tryMove(World world, BlockPos pos, BlockState state, CallbackInfo ci) {
+    @Inject(
+            method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;",
+            at = @At("RETURN")
+    )
+    public void place(ItemPlacementContext context, CallbackInfoReturnable<ActionResult> cir) {
+        int r = 12;
+        BlockPos placePos = context.getBlockPos();
+        PlayerEntity player = context.getPlayer();
+        ItemStack stack = context.getStack();
+
+        if (player != null) {
+            World world = player.getWorld();
+            for (int i = -12; i <= 12; i++) {
+                if (i != 0) {
+                    BlockPos pos1 = new BlockPos(placePos.getX() + i, placePos.getY(), placePos.getZ());
+                    BlockPos pos2 = new BlockPos(placePos.getX(), placePos.getY() + i, placePos.getZ());
+                    BlockPos pos3 = new BlockPos(placePos.getX(), placePos.getY(), placePos.getZ() + i);
+                    hasSlimeBlock(player, world, pos1, stack, placePos);
+                    hasSlimeBlock(player, world, pos2, stack, placePos);
+                    hasSlimeBlock(player, world, pos3, stack, placePos);
+                }
+            }
+        }
+
+    }
+    private void hasSlimeBlock(PlayerEntity player, World world, BlockPos pos, ItemStack stack, BlockPos placePos) {
+        BlockState blockState = world.getBlockState(pos);
+        if (
+                stack.getItem() instanceof BlockItem blockItem
+                        &&
+                        (
+                                blockItem.getBlock() instanceof AbstractRailBlock
+                                        ||
+                                        blockItem.getBlock() instanceof DyedCarpetBlock
+                        )
+        ) {
+            if (blockState.getBlock() instanceof SlimeBlock || blockState.getBlock() instanceof PistonBlock) {
+                world.setBlockState(placePos, Blocks.AIR.getDefaultState());
+                player.giveItemStack(stack);
+
+            }
+        }
+
     }
 
 }
