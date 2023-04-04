2aadb09f691eca6235044c92c209a3c7e5ecbbfe
baka4n <qq474899581@gmail.com>
2023-04-04 22:19:32 +0800

    批量移位

---
+++ 1.19.2/nucleoplasm-api/common/src/main/java/org/overrun/nucleoplasm/api/registry/item/test/ExampleItem.java
@@ -0,0 +1,9 @@
+package org.overrun.nucleoplasm.api.registry.item.test;
+
+import net.minecraft.world.item.Item;
+
+public class ExampleItem extends Item {
+    public ExampleItem(Properties properties) {
+        super(properties);
+    }
+}
