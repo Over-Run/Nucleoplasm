63b1cb424ec91cc32129c971bfaf7804d3869868
baka4n <qq474899581@gmail.com>
2023-04-09 00:28:52 +0800

    更改

--- nucleoplasm/nucleoplasm-chemistry/forge/src/main/java/org/overrun/nucleoplasm_chemistry/forge/Nucleoplasm_chemistryForge.java
+++ nucleoplasm/nucleoplasm-chemistry/forge/src/main/java/org/overrun/nucleoplasm_chemistry/forge/Nucleoplasm_chemistryForge.java
@@ -5,9 +5,9 @@ import org.overrun.nucleoplasm_chemistry.Nucleoplasm_chemistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(Nucleoplasm_nuclide.MOD_ID)
+@Mod(Nucleoplasm_chemistry.MOD_ID)
 public class Nucleoplasm_chemistryForge {
-    public Nucleoplasm_nuclideForge() {
+    public Nucleoplasm_chemistryForge() {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(Nucleoplasm_chemistry.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         Nucleoplasm_chemistry.init();
