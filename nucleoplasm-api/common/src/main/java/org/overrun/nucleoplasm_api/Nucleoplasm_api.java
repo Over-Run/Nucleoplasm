15cbd772de70ccfa3cbd2571ec71abeb44b2bf84
baka4n <60695447+baka4n@users.noreply.github.com>
2023-05-20 14:34:36 +0800

    Support to get version
    Support to get version

--- nucleoplasm-api/common/src/main/java/org/overrun/nucleoplasm_api/Nucleoplasm_api.java
+++ nucleoplasm-api/common/src/main/java/org/overrun/nucleoplasm_api/Nucleoplasm_api.java
@@ -2,6 +2,7 @@ package org.overrun.nucleoplasm_api;
 
 public class Nucleoplasm_api {
     public static final String MOD_ID = "nucleoplasm_api";
+    public static final String version = "1.0.0.0"
     public static void init() {
 
     }
